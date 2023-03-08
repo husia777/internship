caf300cda34dd69791060a753a7f34206af6329c
husein <huseinnaimov@bk.ru>
2023-03-08 14:22:20 +0500

    123

--- config/settings.py
+++ config/settings.py
@@ -53,9 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
