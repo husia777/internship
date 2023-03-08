90169c704a4bf1a67e9ede9d8151dab66b62a0a8
husein <huseinnaimov@bk.ru>
2023-03-08 16:28:39 +0500

    123

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from config import settings
 
@@ -26,5 +27,6 @@ urlpatterns = [
 
 ]
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
