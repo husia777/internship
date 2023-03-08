d2cde251036492f3b004350d9fd60a687d7ff880
husein <huseinnaimov@bk.ru>
2023-03-08 14:45:59 +0500

    123

--- config/settings.py
+++ config/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/www/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/www/media/'
 
 TEMPLATES = [
     {
