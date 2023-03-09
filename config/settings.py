63707a9975a1c77ed9530b03c94798141b7cea06
husein <huseinnaimov@bk.ru>
2023-03-09 11:04:40 +0500

    123

--- config/settings.py
+++ config/settings.py
@@ -57,8 +57,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
